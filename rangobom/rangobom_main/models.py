eba972e28ab396e4ec1489a83dd163d6f1169abe
Daniel <danielvenancioif@gmail.com>
2025-02-28 14:06:37 -0300

    image url workin

--- rangobom/rangobom_main/models.py
+++ rangobom/rangobom_main/models.py
@@ -3,5 +3,6 @@ from django.db import models
 # Create your models here.
 class Company(models.Model):
     name = models.CharField(max_length=50)
+    profile_image_url= models.CharField(max_length=200,null=True)
     latitude = models.CharField(max_length=50)
     longitude = models.CharField(max_length=50)
